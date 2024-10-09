82f76b791caf2d1bace1a6e1d27df9bd46a5789b
mart8763 <mart.vallejos@duocuc.cl>
2024-10-09 20:42:17 -0300

    arreglo

--- MasterBIKE/forms.py
+++ MasterBIKE/forms.py
@@ -10,7 +10,7 @@ class LoginForm(AuthenticationForm):
     
 class CustomerRegistrationForm(UserCreationForm):
     username =  forms.CharField(widget=forms.TextInput(attrs={'autofocus ': 'True', 'class':'form-control'}))
-    email =     forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
+    email =     forms.EmailField(max_length=150, widget=forms.EmailInput(attrs={'class':'form-control'}))
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
     password2 = forms.CharField(label='Confirmar password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
